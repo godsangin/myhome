e6801edf7fb1bbe937f097cda2eeecf399e92e38
godsangin <leein3293@naver.com>
2019-06-04 21:42:53 +0900

    first commit(bootstrap) and starter project

---
+++ MyHome/src/main/java/com/myhome/MyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeApplication.class, args);
+	}
+
+}
