fb4ba49ecec84ea250262297fda1654bca48ac96
godsangin <leein3293@gmail.com>
2019-06-10 17:53:29 +0900

    sample api request implements

---
+++ MyHome/src/main/java/com/myhome/webservice/domain/PostsSaveRequestDto.java
@@ -0,0 +1,23 @@
+package com.myhome.webservice.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
