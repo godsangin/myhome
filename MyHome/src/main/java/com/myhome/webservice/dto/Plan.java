690797898881560594874f131fa59818cb963091
Myeong <maybe8189@naver.com>
2019-09-05 16:10:59 +0900

    plan mapper 변경

--- MyHome/src/main/java/com/myhome/webservice/dto/Plan.java
+++ MyHome/src/main/java/com/myhome/webservice/dto/Plan.java
@@ -12,14 +12,17 @@ public class Plan{
     private String p_sdate;
     private String p_edate;
     private String p_content;
+    private int p_color;
     
     @Builder
-	public Plan(int p_number, String p_title, String p_sdate, String p_edate, String p_content) {
+	public Plan(int p_number, String p_title, String p_sdate, String p_edate, String p_content, int p_color) {
 		super();
 		this.p_number = p_number;
 		this.p_title = p_title;
 		this.p_sdate = p_sdate;
 		this.p_edate = p_edate;
 		this.p_content = p_content;
+		this.p_color = p_color;
+		
 	}
 }
